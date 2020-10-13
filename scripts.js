afe3810a6398ac080f1cd9bf52bf1d69893e1d3e
socopoko <59167401+socopoko@users.noreply.github.com>
2020-10-13 18:52:48 +0530

    added mouse click feature

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const keys = Array.from(document.querySelectorAll('.key'));
+
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
@@ -9,21 +11,18 @@ function setBg() {
   color.innerHTML = "#" + randomColor;
 }
 
-
 function playSound(e) {
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    const keys = Array.from(document.querySelectorAll('.key'));
-    const body = document.getElementsByTagName('body')[0];
-
-    if(!audio) return;
+  const keycode = (e.type == 'keydown') ? e.keyCode: e.path[1].dataset.key;
+  const audio = document.querySelector(`audio[data-key="${keycode}"]`);
+  const key = document.querySelector(`div[data-key="${keycode}"]`);
     
-    key.classList.add('playing');
-    
-    audio.currentTime = 0;
-    audio.play();
-    keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+  if(!audio) return;
+  key.classList.add('playing');
+  audio.currentTime = 0;
+  audio.play();
 }
 
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
-window.addEventListener('keydown', setBg);
+keys.forEach(key => key.addEventListener('click', playSound));
+// window.addEventListener('keydown', setBg);
