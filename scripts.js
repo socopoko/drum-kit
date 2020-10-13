68c7bf98954860b2dc2c58f51c322e4e04399040
socopoko <59167401+socopoko@users.noreply.github.com>
2020-10-13 19:44:53 +0530

    updated css and added social links

--- scripts.js
+++ scripts.js
@@ -1,14 +1,9 @@
 const keys = Array.from(document.querySelectorAll('.key'));
+const heading = document.querySelector('h1');
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
     e.target.classList.remove('playing');
-}
-
-function setBg() {
-  const randomColor = Math.floor(Math.random()*16777215).toString(16);
-  document.body.style.backgroundColor = "#" + randomColor;
-  color.innerHTML = "#" + randomColor;
 }
 
 function playSound(e) {
@@ -18,11 +13,16 @@ function playSound(e) {
     
   if(!audio) return;
   key.classList.add('playing');
+  heading.classList.add('glow');
   audio.currentTime = 0;
   audio.play();
+
+  setTimeout(() => {
+    heading.classList.remove('glow');
+  }, 100);
+  
 }
 
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 window.addEventListener('keydown', playSound);
 keys.forEach(key => key.addEventListener('click', playSound));
-// window.addEventListener('keydown', setBg);
